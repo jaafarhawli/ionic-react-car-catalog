1ebd0c37fa76f39ec825bb568e3c1b69c328ef78
jaafarhawli <jaafarhawle@gmail.com>
2023-04-08 07:48:50 +0300

    app: fetched cars array into car cards on home screen

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,17 +1,23 @@
-import {
-  IonContent,
-  IonPage,
-} from "@ionic/react";
-import ExploreContainer from "../../components/ExploreContainer";
+import { useContext } from "react";
+import { IonContent, IonPage } from "@ionic/react";
 import "./Home.css";
 import Title from "../../components/Title/Title";
+import CarCard from "../../components/CarCard/CarCard";
+import { CarsContext } from "../../context/CarsContext";
 
 const Home: React.FC = () => {
+
+  const { cars } = useContext(CarsContext);
+
   return (
     <IonPage>
       <Title title="Home" />
-      <IonContent fullscreen>   
-        <ExploreContainer />
+      <IonContent fullscreen>
+        {
+          cars.map(car => {
+            return <CarCard key={car.id} name={car.name} description={car.description} model={car.model} year={car.year} />
+          })
+        }
       </IonContent>
     </IonPage>
   );
