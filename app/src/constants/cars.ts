6b8e097b98ca67122fc235a291b8b217e0ae4c1f
jaafarhawli <jaafarhawle@gmail.com>
2023-04-08 05:11:32 +0300

    app: added an array of dummy car objects

---
+++ app/src/constants/cars.ts
@@ -0,0 +1,397 @@
+export const cars = [
+    {
+      name: "Audi A4",
+      description: "Luxury sedan with all-wheel drive",
+      model: "A4",
+      year: 2007,
+    },
+    {
+      name: "BMW 3 Series",
+      description: "Sporty sedan with rear-wheel drive",
+      model: "3 Series",
+      year: 2008,
+    },
+    {
+      name: "Chevrolet Malibu",
+      description: "Affordable midsize sedan",
+      model: "Malibu",
+      year: 2004,
+    },
+    {
+      name: "Dodge Durango",
+      description: "Powerful SUV with spacious interior",
+      model: "Durango",
+      year: 2011,
+    },
+    {
+      name: "Ford Fusion",
+      description: "Popular midsize sedan",
+      model: "Fusion",
+      year: 2010,
+    },
+    {
+      name: "GMC Yukon",
+      description: "Large SUV with plenty of cargo space",
+      model: "Yukon",
+      year: 2005,
+    },
+    {
+      name: "Honda Accord",
+      description: "Reliable and efficient midsize sedan",
+      model: "Accord",
+      year: 2006,
+    },
+    {
+      name: "Infiniti G35",
+      description: "Sporty luxury sedan with rear-wheel drive",
+      model: "G35",
+      year: 2003,
+    },
+    {
+      name: "Jeep Wrangler",
+      description: "Iconic off-road vehicle",
+      model: "Wrangler",
+      year: 2009,
+    },
+    {
+      name: "Kia Optima",
+      description: "Midsize sedan with great fuel economy",
+      model: "Optima",
+      year: 2007,
+    },
+    {
+      name: "Lexus ES",
+      description: "Luxury sedan with comfortable ride",
+      model: "ES",
+      year: 2012,
+    },
+    {
+      name: "Mazda Mazda3",
+      description: "Fun-to-drive compact car",
+      model: "Mazda3",
+      year: 2008,
+    },
+    {
+      name: "Nissan Altima",
+      description: "Reliable midsize sedan with spacious interior",
+      model: "Altima",
+      year: 2005,
+    },
+    {
+      name: "Oldsmobile Alero",
+      description: "Affordable midsize car",
+      model: "Alero",
+      year: 2002,
+    },
+    {
+      name: "Pontiac Grand Prix",
+      description: "Sporty midsize car",
+      model: "Grand Prix",
+      year: 2004,
+    },
+    {
+      name: "Saturn Ion",
+      description: "Compact car with good fuel economy",
+      model: "Ion",
+      year: 2007,
+    },
+    {
+      name: "Toyota Camry",
+      description: "Reliable midsize sedan",
+      model: "Camry",
+      year: 2009,
+    },
+    {
+      name: "Volkswagen Jetta",
+      description: "German-engineered compact car",
+      model: "Jetta",
+      year: 2006,
+    },
+    {
+      name: "Acura TL",
+      description: "Luxury sedan with great performance",
+      model: "TL",
+      year: 2004,
+    },
+    {
+        name: "Honda Accord",
+        description: "A comfortable, spacious sedan with good handling",
+        model: "LX",
+        year: 2006,
+      },
+      {
+        name: "Nissan Altima",
+        description: "A midsize sedan with good fuel economy and a spacious interior",
+        model: "S",
+        year: 2008,
+      },
+      {
+        name: "Ford Mustang",
+        description: "A sporty, powerful muscle car",
+        model: "GT",
+        year: 2012,
+      },
+      {
+        name: "Chevrolet Corvette",
+        description: "A high-performance sports car with impressive handling",
+        model: "Z06",
+        year: 2007,
+      },
+      {
+        name: "Dodge Charger",
+        description: "A large, powerful sedan with aggressive styling",
+        model: "R/T",
+        year: 2010,
+      },
+      {
+        name: "Chrysler 300",
+        description: "A spacious luxury sedan with a comfortable ride",
+        model: "Limited",
+        year: 2005,
+      },
+      {
+        name: "Audi A4",
+        description: "A refined, high-performance luxury sedan",
+        model: "2.0T",
+        year: 2011,
+      },
+      {
+        name: "BMW 3 Series",
+        description: "A sporty, luxurious compact car",
+        model: "328i",
+        year: 2008,
+      },
+      {
+        name: "Mercedes-Benz C-Class",
+        description: "A stylish, high-performance luxury sedan",
+        model: "C250",
+        year: 2012,
+      },
+      {
+        name: "Volkswagen Jetta",
+        description: "A fuel-efficient, comfortable compact car",
+        model: "SE",
+        year: 2006,
+      },
+      {
+        name: "Volvo S60",
+        description: "A refined, comfortable luxury sedan with good safety ratings",
+        model: "T6",
+        year: 2010,
+      },
+      {
+        name: "Subaru Impreza",
+        description: "A reliable, fuel-efficient compact car",
+        model: "2.5i",
+        year: 2007,
+      },
+      {
+        name: "Mazda Mazda3",
+        description: "A sporty, fun-to-drive compact car",
+        model: "s Grand Touring",
+        year: 2012,
+      },
+      {
+        name: "Hyundai Sonata",
+        description: "A stylish, fuel-efficient midsize sedan",
+        model: "GLS",
+        year: 2011,
+      },
+      {
+        name: "Kia Optima",
+        description: "A spacious, well-equipped midsize sedan",
+        model: "LX",
+        year: 2009,
+      },
+      {
+        name: "Lexus LS",
+        description: "A refined, comfortable luxury sedan",
+        model: "460",
+        year: 2010,
+      },
+      {
+        name: "Infiniti G35",
+        description: "A sporty, luxurious compact car",
+        model: "Journey",
+        year: 2007,
+      },
+      {
+        name: "BMW X5",
+        description: "Luxury midsize SUV",
+        model: "X5",
+        year: 2005,
+      },
+      {
+        name: "Mercedes-Benz C-Class",
+        description: "Luxury compact car",
+        model: "C-Class",
+        year: 2007,
+      },
+      {
+        name: "Ford Mustang",
+        description: "Sports car",
+        model: "Mustang GT",
+        year: 2006,
+      },
+      {
+        name: "Chevrolet Corvette",
+        description: "Sports car",
+        model: "Corvette C6",
+        year: 2007,
+      },
+      {
+        name: "Toyota Camry",
+        description: "Midsize car",
+        model: "Camry LE",
+        year: 2003,
+      },
+      {
+        name: "Honda Civic",
+        description: "Compact car",
+        model: "Civic LX",
+        year: 2005,
+      },
+      {
+        name: "Nissan Altima",
+        description: "Midsize car",
+        model: "Altima 2.5",
+        year: 2012,
+      },
+      {
+        name: "Audi A4",
+        description: "Luxury compact car",
+        model: "A4",
+        year: 2009,
+      },
+      {
+        name: "Volvo S60",
+        description: "Luxury midsize car",
+        model: "S60 T6",
+        year: 2011,
+      },
+      {
+        name: "Lexus RX",
+        description: "Luxury midsize SUV",
+        model: "RX 350",
+        year: 2010,
+      },
+      {
+        name: "Infiniti G35",
+        description: "Luxury compact car",
+        model: "G35 Coupe",
+        year: 2004,
+      },
+      {
+        name: "Mitsubishi Lancer",
+        description: "Compact car",
+        model: "Lancer ES",
+        year: 2010,
+      },
+      {
+        name: "Subaru Impreza",
+        description: "Compact car",
+        model: "Impreza WRX",
+        year: 2008,
+      },
+      {
+        name: "Jeep Grand Cherokee",
+        description: "Midsize SUV",
+        model: "Grand Cherokee Laredo",
+        year: 2001,
+      },
+      {
+        name: "Dodge Charger",
+        description: "Muscle car",
+        model: "Charger R/T",
+        year: 2012,
+      },
+      {
+        name: "Pontiac GTO",
+        description: "Muscle car",
+        model: "GTO",
+        year: 2006,
+      },
+      {
+        name: "GMC Sierra",
+        description: "Full-size pickup truck",
+        model: "Sierra 1500",
+        year: 2011,
+      },
+      {
+        name: "Ford F-150",
+        description: "Full-size pickup truck",
+        model: "F-150 XL",
+        year: 2005,
+      },
+      {
+        name: "Chevrolet Silverado",
+        description: "Full-size pickup truck",
+        model: "Silverado 1500",
+        year: 2009,
+      },
+      {
+        name: "Toyota Tundra",
+        description: "Full-size pickup truck",
+        model: "Tundra SR5",
+        year: 2010,
+      },
+      {
+        name: "Audi A4",
+        description: "A luxury compact sedan.",
+        model: "A4",
+        year: 2006
+      },
+      {
+        name: "Toyota Camry",
+        description: "A reliable and popular midsize sedan.",
+        model: "Camry",
+        year: 2007
+      },
+      {
+        name: "Honda Civic",
+        description: "A fuel-efficient and practical compact car.",
+        model: "Civic",
+        year: 2003
+      },
+      {
+        name: "Mercedes-Benz S-Class",
+        description: "A luxury full-size sedan with advanced technology.",
+        model: "S-Class",
+        year: 2012
+      },
+      {
+        name: "Nissan Altima",
+        description: "A popular midsize sedan with good fuel economy.",
+        model: "Altima",
+        year: 2010
+      },
+      {
+        name: "Ford F-150",
+        description: "A popular full-size pickup truck with a variety of features.",
+        model: "F-150",
+        year: 2005
+      },
+      {
+        name: "Chevrolet Silverado",
+        description: "A reliable full-size pickup truck with a spacious cabin.",
+        model: "Silverado",
+        year: 2011
+      },
+      {
+        name: "Dodge Charger",
+        description: "A high-performance full-size sedan with a muscular design.",
+        model: "Charger",
+        year: 2009
+      },
+      {
+        name: "Mazda 3",
+        description: "A stylish and fun-to-drive compact car.",
+        model: "3",
+        year: 2008
+      },
+      {
+        name: "Volkswagen Passat",
+        description: "A comfortable midsize sedan with good handling.",
+        model: "Passat",
+        year: 2002
+      }]
