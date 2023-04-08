cca58602861a33d7957207ae19ce937c636ee0fb
jaafarhawli <jaafarhawle@gmail.com>
2023-04-08 09:24:15 +0300

    app: added modal component

---
+++ app/src/components/Modal/Modal.tsx
@@ -0,0 +1,28 @@
+import { IonModal, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonContent,  } from '@ionic/react'
+
+type Props = {
+    isOpen: boolean,
+    onClose: () => void,
+    title: string,
+    children: any
+}
+
+const Modal = (props: Props) => {
+  return (
+    <IonModal isOpen={props.isOpen}>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>{props.title}</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => props.onClose}>Close</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            {props.children}
+          </IonContent>
+        </IonModal>
+  )
+}
+
+export default Modal
